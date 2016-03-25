ece4404ce303c777c6b97188d60c6a107c3d6c38
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-03-25 15:14:51 -0300

    Pop up portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
 
-	function scrollMenu(){
+	(function scrollMenu(){
 		$('nav.egg-menu ul li a').on('click', function(){
 			var selectID = $(this).attr('href');
-			
+
 			$('html, body').animate({
 				scrollTop: $(selectID).offset().top
 			}, 1000);
-			
+
 			return false;
 		});
-	}
+	})();
 
-	scrollMenu();
-
+	(function togglePopUp(){
+		$('.egg-group-portfolio ul > li > a').on('click', function(){
+			$(this).parent().find('.egg-fixed').addClass('active').fadeIn(700);
+			return false;
+		});
+		$('.egg-close').on('click', function(){
+			$('.egg-fixed').fadeOut(700);
+		});
+	})();
 
 	// calling wow.js
 	wow = new WOW({
